787d640c2488c22c30d9804fa819d66c05006d7a
PolianaSouzaAnanias <poliana.souzaa@hotmail.com>
2021-10-26 14:23:46 -0300

    Criando vairáveis

--- Curso-Java-parte-1-Primeiros-passos-capitulo2/Curso-Java-parte-1-Primeiros-passos-capitulo2/Programa.java
+++ Curso-Java-parte-1-Primeiros-passos-capitulo2/Curso-Java-parte-1-Primeiros-passos-capitulo2/Programa.java
@@ -2,6 +2,6 @@ public class Programa{
 
     public static void main(String[] args){
 
-        System.out.println("ola mundo");
+        System.out.println("ola, mundo!");
     }
 }
---
+++ Curso-Java-parte-1-Primeiros-passos-capitulo2/Curso-Java-parte-1-Primeiros-passos-capitulo2/testaVariaveis.java
@@ -0,0 +1,12 @@
+
+public class testaVariaveis {
+
+	public static void main(String[] args) {
+      System.out.println("ola novo teste"); 
+      
+      int idade;
+      idade = 37; // Outro exemplo para criar variárel, na mesmo linha. int idade = 37;
+      
+      System.out.println("Sua idade é " + idade); // Uso de concatenação +
+	}
+}
