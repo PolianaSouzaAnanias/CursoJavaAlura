9f8792a2e1a129d5042cec0d882dc902e1a34b5f
PolianaSouzaAnanias <poliana.souzaa@hotmail.com>
2021-10-27 09:27:24 -0300

    Praticando while

--- Curso-Java-parte-1-Primeiros-passos-capitulo2/Curso-Java-parte-1-Primeiros-passos-capitulo2/TestaWhile.java
+++ Curso-Java-parte-1-Primeiros-passos-capitulo2/Curso-Java-parte-1-Primeiros-passos-capitulo2/TestaWhile.java
@@ -7,6 +7,7 @@ public class TestaWhile {
 	        while(contador <= 10) {
 	            System.out.println(contador);
 	            contador = contador + 1; // Para o mesmo efeito, existe ainda o ++: contador++;
+// Esta, na verdade, é a forma mais comum de se somar o valor de si mesmo mais uma vez, e usar ++contador; (o pré-incremento) também traria o mesmo resultado.
 	        }
 	    }
 	}
